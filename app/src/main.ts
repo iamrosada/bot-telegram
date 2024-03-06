49c849327477a2861ca7a126cb99e5342e2e5a53
iamrosada <luisrosada2018@gmail.com>
2024-03-06 19:06:11 +0300

    feat: added prisma on code

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,13 +1,35 @@
 import { Telegraf, Context } from "telegraf";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const bot = new Telegraf("7147070611:AAGA3RzbN44mFiRyTQ7cBR0jpU-4hW390IY");
 
 // Middleware to send a hello message to the user
-bot.use((ctx: Context, next) => {
+bot.use(async (ctx: Context, next) => {
   ctx.replyWithSticker(
     "CAACAgIAAxkBAUKlF2XoPUIP_WasegbILrEf8yUlOBpWAAIZAAPp2BMoV2ES2mxgqss0BA"
   ); // Adicione o ID do arquivo do sticker aqui
   ctx.reply('Hello! Please send "hello" to start.');
+  const user = await prisma.user.findUnique({
+    where: { idTelegram: ctx.from?.id.toString() },
+  });
+
+  if (!user) {
+    await prisma.user.create({
+      data: {
+        idTelegram: ctx.from?.id.toString() as any,
+        userName: ctx.from?.username || "",
+        fullName: `${ctx.from?.first_name || ""} ${ctx.from?.last_name || ""}`,
+        plan: {
+          create: {
+            name: "Free", // Defina o splano padrão para os novos usuários aqui
+          },
+        },
+      },
+    });
+  }
+
+  await next();
   next();
 });
 
@@ -48,28 +70,66 @@ bot.action("choose_plan", async (ctx: Context) => {
 
 bot.action("free_option", async (ctx: Context) => {
   if (ctx.callbackQuery) {
-    ctx.replyWithSticker(
+    await ctx.replyWithSticker(
       "CAACAgIAAxkBAUKlF2XoPUIP_WasegbILrEf8yUlOBpWAAIZAAPp2BMoV2ES2mxgqss0BA"
-    ); // Adicione o ID do arquivo do sticker aqui
-    await ctx.reply(
-      "Thank you for choosing the free option. Activating your plan..."
     );
-    await sleep(15000); // Wait 15 seconds
-    await ctx.reply("Your free plan is now active!");
+
+    const user = await prisma.user.findUnique({
+      where: { idTelegram: ctx.from?.id.toString() },
+      include: { plan: true },
+    });
+
+    if (user && user.plan.name !== "Free") {
+      await prisma.user.update({
+        where: { idTelegram: ctx.from?.id.toString() },
+        data: {
+          plan: {
+            update: { name: "Free" },
+          },
+        },
+      });
+
+      await ctx.reply(
+        "Thank you for choosing the free option. Activating your plan..."
+      );
+      await sleep(15000); // Wait 15 seconds
+      await ctx.reply("Your free plan is now active!");
+    } else {
+      await ctx.reply("You are already on the Free plan.");
+    }
   }
 });
 
 bot.action(["basic_plan", "advanced_plan"], async (ctx: Context | any) => {
   if (ctx.callbackQuery) {
-    ctx.replyWithSticker(
+    await ctx.replyWithSticker(
       "CAACAgIAAxkBAUKlF2XoPUIP_WasegbILrEf8yUlOBpWAAIZAAPp2BMoV2ES2mxgqss0BA"
-    ); // Adicione o ID do arquivo do sticker aqui
-    const plan = ctx.callbackQuery.data;
-    await ctx.reply(
-      `Thank you for choosing the ${plan} plan. Activating your plan...`
     );
-    await sleep(15000); // Wait 15 seconds
-    await ctx.reply(`Your ${plan} plan is now active!`);
+
+    const plan = ctx.callbackQuery.data;
+    const user = await prisma.user.findUnique({
+      where: { idTelegram: ctx.from?.id.toString() },
+      include: { plan: true },
+    });
+
+    if (user && user.plan.name !== plan) {
+      await prisma.user.update({
+        where: { idTelegram: ctx.from?.id.toString() },
+        data: {
+          plan: {
+            update: { name: plan },
+          },
+        },
+      });
+
+      await ctx.reply(
+        `Thank you for choosing the ${plan} plan. Activating your plan...`
+      );
+      await sleep(15000); // Wait 15 seconds
+      await ctx.reply(`Your ${plan} plan is now active!`);
+    } else {
+      await ctx.reply(`You are already on the ${plan} plan.`);
+    }
   }
 });
 

d59faf35cae39f6fdaca5370f245c3498e043092
iamrosada <luisrosada2018@gmail.com>
2024-03-06 12:59:32 +0300

    feat: added sticker for each message

--- app/src/main.ts
+++ app/src/main.ts
@@ -4,12 +4,18 @@ const bot = new Telegraf("7147070611:AAGA3RzbN44mFiRyTQ7cBR0jpU-4hW390IY");
 
 // Middleware to send a hello message to the user
 bot.use((ctx: Context, next) => {
+  ctx.replyWithSticker(
+    "CAACAgIAAxkBAUKlF2XoPUIP_WasegbILrEf8yUlOBpWAAIZAAPp2BMoV2ES2mxgqss0BA"
+  ); // Adicione o ID do arquivo do sticker aqui
   ctx.reply('Hello! Please send "hello" to start.');
   next();
 });
 
 // Handling the user's response
 bot.hears("hello", async (ctx: Context) => {
+  ctx.replyWithSticker(
+    "CAACAgIAAxkBAUKlF2XoPUIP_WasegbILrEf8yUlOBpWAAIZAAPp2BMoV2ES2mxgqss0BA"
+  ); // Adicione o ID do arquivo do sticker aqui
   await ctx.reply("Great! Choose an option:", {
     reply_markup: {
       inline_keyboard: [
@@ -24,6 +30,9 @@ bot.hears("hello", async (ctx: Context) => {
 
 // Handling button clicks
 bot.action("choose_plan", async (ctx: Context) => {
+  ctx.replyWithSticker(
+    "CAACAgIAAxkBAUKlF2XoPUIP_WasegbILrEf8yUlOBpWAAIZAAPp2BMoV2ES2mxgqss0BA"
+  ); // Adicione o ID do arquivo do sticker aqui
   await ctx.reply("Please choose a plan:", {
     reply_markup: {
       inline_keyboard: [
@@ -39,6 +48,9 @@ bot.action("choose_plan", async (ctx: Context) => {
 
 bot.action("free_option", async (ctx: Context) => {
   if (ctx.callbackQuery) {
+    ctx.replyWithSticker(
+      "CAACAgIAAxkBAUKlF2XoPUIP_WasegbILrEf8yUlOBpWAAIZAAPp2BMoV2ES2mxgqss0BA"
+    ); // Adicione o ID do arquivo do sticker aqui
     await ctx.reply(
       "Thank you for choosing the free option. Activating your plan..."
     );
@@ -49,6 +61,9 @@ bot.action("free_option", async (ctx: Context) => {
 
 bot.action(["basic_plan", "advanced_plan"], async (ctx: Context | any) => {
   if (ctx.callbackQuery) {
+    ctx.replyWithSticker(
+      "CAACAgIAAxkBAUKlF2XoPUIP_WasegbILrEf8yUlOBpWAAIZAAPp2BMoV2ES2mxgqss0BA"
+    ); // Adicione o ID do arquivo do sticker aqui
     const plan = ctx.callbackQuery.data;
     await ctx.reply(
       `Thank you for choosing the ${plan} plan. Activating your plan...`
@@ -64,6 +79,69 @@ function sleep(ms: number) {
 }
 
 bot.launch();
+
+// Middleware to send a hello message to the user
+// bot.use((ctx: Context, next) => {
+//   ctx.reply('Hello! Please send "hello" to start.');
+//   next();
+// });
+
+// // Handling the user's response
+// bot.hears("hello", async (ctx: Context) => {
+//   await ctx.reply("Great! Choose an option:", {
+//     reply_markup: {
+//       inline_keyboard: [
+//         [
+//           { text: "Choose Plan", callback_data: "choose_plan" },
+//           { text: "Free Option", callback_data: "free_option" },
+//         ],
+//       ],
+//     },
+//   });
+// });
+
+// // Handling button clicks
+// bot.action("choose_plan", async (ctx: Context) => {
+//   await ctx.reply("Please choose a plan:", {
+//     reply_markup: {
+//       inline_keyboard: [
+//         [
+//           { text: "Basic Plan", callback_data: "basic_plan" },
+//           { text: "Advanced Plan", callback_data: "advanced_plan" },
+//           { text: "Free Plan", callback_data: "free_plan" },
+//         ],
+//       ],
+//     },
+//   });
+// });
+
+// bot.action("free_option", async (ctx: Context) => {
+//   if (ctx.callbackQuery) {
+//     await ctx.reply(
+//       "Thank you for choosing the free option. Activating your plan..."
+//     );
+//     await sleep(15000); // Wait 15 seconds
+//     await ctx.reply("Your free plan is now active!");
+//   }
+// });
+
+// bot.action(["basic_plan", "advanced_plan"], async (ctx: Context | any) => {
+//   if (ctx.callbackQuery) {
+//     const plan = ctx.callbackQuery.data;
+//     await ctx.reply(
+//       `Thank you for choosing the ${plan} plan. Activating your plan...`
+//     );
+//     await sleep(15000); // Wait 15 seconds
+//     await ctx.reply(`Your ${plan} plan is now active!`);
+//   }
+// });
+
+// // Helper function to sleep
+// function sleep(ms: number) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+
+// bot.launch();
 
 // import * as amqp from "amqplib";
 

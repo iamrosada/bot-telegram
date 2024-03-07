056e82b7ec1711425ce3b33210012a7c74d03006
iamrosada <luisrosada2018@gmail.com>
2024-03-07 19:57:53 +0300

    feat: created the ProductsRepository

---
+++ app/purchase/src/application/repositories/products-repository.ts
@@ -0,0 +1,6 @@
+import { Product } from "../../domain/product";
+
+export interface ProductsRepository {
+  findById(id: string): Promise<Product | null>;
+  create(title: string): Promise<Product | null>;
+}
